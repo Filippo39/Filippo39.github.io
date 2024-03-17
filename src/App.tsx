ea86c4bd109ac563d67c0e6d5f954bc8415d2829
Filippo Mantovan <filippomantovan39@gmail.com>
2024-03-17 11:28:33 +0100

    add: favicon + navbar + styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works } from './components'
+import { Stars } from './components/ui'
 
 function App() {
   return (
-    <>
-      <div>Test</div>
-    </>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+
+        <div className="relative z-0">
+          <Contact />
+          <Stars />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
